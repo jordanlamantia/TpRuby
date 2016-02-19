69483a599c4b245e259bf3533f87ec3e6859f043
jordanlamantia <jordan.lamantia@eemi.com>
2016-02-19 23:32:57 +0100

    page signup + route

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Ruby"
+    if page_title.empty?
+      page_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
