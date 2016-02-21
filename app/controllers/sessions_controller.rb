6fe5ba497910d4801d64c49553503c231f85b84d
jordanlamantia <jordan.lamantia@eemi.com>
2016-02-21 15:20:56 +0100

    Log in user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Log le user et le redirige vers ça page
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      flash[:success] = 'Well done your now loged in'
+      redirect_to user
+    else
+      #Affichage des erreurs
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password:              "foo",
                                password_confirmation: "bar" }
     end
-    assert_template 'users/new'
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
